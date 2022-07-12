6d274fa4ed5ee87a4fc47336e058fa61727b5dfe
John <760048@cognizant.com>
2022-07-12 15:51:07 +0530

    added parameters

--- lambda/user.py
+++ lambda/user.py
@@ -9,12 +9,11 @@ def lambda_handler(event, context):
     print("Received event ",event)
     # print (event.field)
     # print (event.arguments)
-    user = {
-            'userid': userid,
-            'usertype': usertype,
-            'username': uname,
-            'points':point
-    }
+    userid = uuid.uuid4()
+    usertype = event["usertype"]
+    uname = event["uname"]
+    point = event["point"]
+    
     
     dynamodb = boto3.resource(
         'dynamodb', endpoint_url="https://dynamodb.us-east-1.amazonaws.com")
@@ -22,7 +21,12 @@ def lambda_handler(event, context):
     users_table = dynamodb.Table('Users')
     response = users_table.put_item(
         # Data to be inserted
-        Item=user
+        Item={
+            'userid': str(userid),
+            'usertype': usertype,
+            'username': uname,
+            'points':point
+        }
     )
     print(response)
     return (response)
