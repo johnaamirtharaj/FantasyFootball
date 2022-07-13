68f04924e9d794f7eb1dbc215d9a869fe7116f29
John <760048@cognizant.com>
2022-07-13 17:46:52 +0530

    kinesis integration done

--- lambda/user.py
+++ lambda/user.py
@@ -1,32 +1,41 @@
 import boto3
 import uuid
-
+import json
+import base64
 
 
 def lambda_handler(event, context):
     # TODO implement
     #exports.handler = async (event) => {
     print("Received event ",event)
-    # print (event.field)
-    # print (event.arguments)
-    userid = uuid.uuid4()
-    usertype = event["usertype"]
-    uname = event["uname"]
-    point = event["point"]
-    
-    
-    dynamodb = boto3.resource(
-        'dynamodb', endpoint_url="https://dynamodb.us-east-1.amazonaws.com")
-    # Specify the table
-    users_table = dynamodb.Table('Users')
-    response = users_table.put_item(
-        # Data to be inserted
-        Item={
-            'userid': str(userid),
-            'usertype': usertype,
-            'username': uname,
-            'points':point
-        }
-    )
-    print(response)
-    return (response)
+    for i in event['Records']:
+        print(i)
+        print(i["kinesis"]["data"])
+        enc = i["kinesis"]["data"]
+        dec = base64.b64decode(enc)
+        print(dec.decode('utf-8'))
+        dec = dec.decode('utf-8')
+        event = json.loads(dec)
+		# print (event.field)
+		# print (event.arguments)
+		userid = uuid.uuid4()
+		usertype = event["usertype"]
+		uname = event["uname"]
+		point = event["point"]
+		
+		
+		dynamodb = boto3.resource(
+			'dynamodb', endpoint_url="https://dynamodb.us-east-1.amazonaws.com")
+		# Specify the table
+		users_table = dynamodb.Table('Users')
+		response = users_table.put_item(
+			# Data to be inserted
+			Item={
+				'userid': str(userid),
+				'usertype': usertype,
+				'username': uname,
+				'points':point
+			}
+		)
+		print(response)
+		return (response)
